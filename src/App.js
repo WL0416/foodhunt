7709b5e855334fafcf319372a871822770bcd038
Li Wei <liwsuper7@gmail.com>
2019-04-27 15:36:30 +1000

    add header to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header branding="Food Hunt" />
         <div className="container" />
       </div>
     );
