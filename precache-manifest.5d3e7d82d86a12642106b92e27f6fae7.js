9e8edd912ae74413c0d0dde1c2fec2e45d576f89
Li Wei <liwsuper7@gmail.com>
2019-04-28 22:58:41 +1000

    Updates

---
+++ precache-manifest.5d3e7d82d86a12642106b92e27f6fae7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a78c2c75c5334e52e14dff390f1cca",
+    "url": "/foodhunt/index.html"
+  },
+  {
+    "revision": "6887721f6c1d2eafc7ec",
+    "url": "/foodhunt/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c26ac4aff063d1788992",
+    "url": "/foodhunt/static/css/main.8761743c.chunk.css"
+  },
+  {
+    "revision": "6887721f6c1d2eafc7ec",
+    "url": "/foodhunt/static/js/2.ec1f7a09.chunk.js"
+  },
+  {
+    "revision": "c26ac4aff063d1788992",
+    "url": "/foodhunt/static/js/main.eae95599.chunk.js"
+  },
+  {
+    "revision": "f35d8da0b5c86d0e9869",
+    "url": "/foodhunt/static/js/runtime~main.887cda07.js"
+  },
+  {
+    "revision": "ec54f1ab22fa237d1a663e4c967efbd5",
+    "url": "/foodhunt/static/media/slide1.ec54f1ab.jpg"
+  },
+  {
+    "revision": "52bb45871faf2f61201d144f04729909",
+    "url": "/foodhunt/static/media/slide2.52bb4587.jpg"
+  },
+  {
+    "revision": "170fbc69dfd96d890be0b7cba6fa4ded",
+    "url": "/foodhunt/static/media/slide3.170fbc69.jpg"
+  }
+]);
