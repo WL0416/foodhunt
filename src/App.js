361a984f9d7cd15a8e43723a7d5374625d855200
Li Wei <liwsuper7@gmail.com>
2019-04-29 23:49:27 +1000

    complete footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
 
 class App extends Component {
@@ -12,11 +13,11 @@ class App extends Component {
         <div className="App">
           <Header branding="Food Hunt" />
           <div className="container">
-            <Home />
             <Switch>
-              <Router exact path="/" component={Home} />
+              <Route exact path="/" component={Home} />
             </Switch>
           </div>
+          <Footer />
         </div>
       </Router>
     );
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Footer = props => {
+  const { copyright } = props;
+  return (
+    <footer className="page-footer font-small danger-color pt-4">
+      <div className="container-fluid text-center text-md-left">
+        <div className="row">
+          <div className="col-md-6 mt-md-0 mt-3">
+            <h5 className="text-uppercase text-center">Footer Content</h5>
+            <p className="text-center">aksoajdijoiwajdijawijdioaj</p>
+          </div>
+          <hr className="clearfix w-100 d-md-none pb-3" />
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="/">Link 1</a>
+              </li>
+              <li>
+                <a href="/">Link 2</a>
+              </li>
+              <li>
+                <a href="/">Link 3</a>
+              </li>
+              <li>
+                <a href="/">Link 4</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-md-3 mb-md-0 mb-3">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="/">Link 1</a>
+              </li>
+              <li>
+                <a href="/">Link 2</a>
+              </li>
+              <li>
+                <a href="/">Link 3</a>
+              </li>
+              <li>
+                <a href="/">Link 4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-copyright text-center py-3">{copyright}</div>
+    </footer>
+  );
+};
+
+Footer.defaultProps = {
+  copyright: " © 2019 Copyright: FoodHunt Pty Ltd"
+};
+
+Footer.propTypes = {
+  copyright: PropTypes.string.isRequired
+};
+
+export default Footer;
