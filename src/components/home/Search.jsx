00d4cdc364bbdd009e8bb61d78945e356adf3229
Li Wei <liwsuper7@gmail.com>
2019-05-04 09:32:28 +1000

    search component start

--- src/components/home/Search.jsx
+++ src/components/home/Search.jsx
@@ -1,1 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <section
+        className="search-banner text-white py-3 form-arka-plan"
+        id="search-banner"
+      >
+        <div className="container py-5 my-5">
+          <div className="row text-center pb-4">
+            <div className="col-md-12">
+              <h2 className="text-white siyad-cerceve">check,check,check</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card acik-renk-form">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <select name="" id="iller" className="form-control">
+                          <option value="" selected>
+                            1
+                          </option>
+                          <option value="">2</option>
+                          <option value="">3</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SearchBox;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,9 +2,15 @@ import React from "react";
 // import PropTypes from "prop-types";
 // import { Link } from "react-rourter-dom";
 import Carousel from "../home/Carousel";
+import Search from "../home/Search";
 
 const Home = props => {
-  return <Carousel />;
+  return (
+    <span>
+      <Carousel />
+      <Search />
+    </span>
+  );
 };
 
 export default Home;
