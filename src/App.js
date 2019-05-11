8d375512f85f6d480f1e9a48796cb8da2d01a969
Li Wei <liwsuper7@gmail.com>
2019-05-11 16:25:49 +1000

    add hotjar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Provider from "./context";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
@@ -9,17 +10,19 @@ import Home from "./components/pages/Home";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header branding="Food Hunt" />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-            </Switch>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Food Hunt" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/context.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const Context = React.createContext();
+
+class Provider extends Component {
+  state = {
+    vendors: []
+  };
+
+  async componentDidMount() {
+    const res = await axios.get(
+      "https://my-json-server.typicode.com/wl0416/JSONapi/db/"
+    );
+
+    this.setState({ vendors: res.data });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Provider;
+export const Consumer = Context.Consumer;
