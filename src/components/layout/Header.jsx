a5d571687022791f78786fce028e20513ded7ff0
Wei Li <Will@Newton-iMac.local>
2019-05-31 14:58:05 +1000

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,7 +17,7 @@ const Header = props => {
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link">
+              <Link to="/today" className="nav-link">
                 TODAY
               </Link>
             </li>
